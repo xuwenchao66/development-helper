43fbf33ef46102d8e9f09ce5ba26ce5a8e0ccb98
xuwenchao <842390367@qq.com>
2022-02-27 21:34:15 +0800

    feat: 🎸 padding for input & output

--- src/pages/JsonParse/Container.tsx
+++ src/pages/JsonParse/Container.tsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components'
+import { BLOCK_PADDING } from '@/styles/theme'
 
 const Wrapper = styled.div`
   display: flex;
   flex: 1;
   flex-direction: row;
+  & .ant-card:nth-child(1) {
+    margin-right: ${BLOCK_PADDING / 2}px;
+  }
 `
 
 export default Wrapper
